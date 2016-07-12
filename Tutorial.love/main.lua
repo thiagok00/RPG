44c18f8950219d2d66d41b3a2660bdf2da6eac8f
thiagok00 <thiagokda1@gmail.com>
2016-07-12 17:42:36 -0300

    Enemies

--- Tutorial.love/main.lua
+++ Tutorial.love/main.lua
@@ -1,3 +1,7 @@
+enemies_controller = {}
+enemies_controller.enemies = {}
+enemy = {}
+enemies_controller.image = love.graphics.newImage("enemy.png")
 
 function love.load()
 	player = {}
@@ -19,7 +23,33 @@ function love.load()
 				table.insert(player.bullets, bullet)
 			end
 		end
+  enemies_controller:spawnEnemy(50,0)
+  enemies_controller:spawnEnemy(230,0)
+
 end
+
+function enemies_controller:spawnEnemy(x,y)
+  enemy = {}
+  enemy.x = x
+  enemy.y = y
+  enemy.bullets = {}
+  enemy.cooldown = 0
+  enemy.speed = 5
+  table.insert(self.enemies,enemy)
+end
+
+function enemy:fire()
+			if self.cooldown <= 0 then
+				self = {}
+				self.width = 10
+				self.height = 10
+				self.x = self.x + self.width/2 - self.width/2
+				self.y = self.y 
+				self.cooldown = 20
+				self.insert(self.bullets, bullet)
+			end
+  end
+
 
 function love.update(dt)
 	if player.cooldown > 0 then
@@ -49,18 +79,32 @@ function love.update(dt)
 		end
 		b.y = b.y - 5
 	end
+  
+  for _,e in pairs(enemies_controller.enemies) do 
+    e.y = e.y + 1
+  end
 
 
 end
 
 
 function love.draw()
-	-- Creating rectangle
+	-- Player
 	love.graphics.setColor(46,20,150)
 	love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
 
+  -- Bullets
 	love.graphics.setColor(255,255,255)
 	for _,b in pairs(player.bullets) do
 		love.graphics.rectangle("fill",b.x,b.y,10,10)
 	end
+  
+  -- Enemies 
+  love.graphics.setColor(255,0,0)
+  for _,e in pairs(enemies_controller.enemies) do
+    		love.graphics.draw(enemies_controller.image,e.x,e.y,40,40)
+  end
+  
+  
+  
 end
